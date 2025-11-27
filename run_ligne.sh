e9ea3b4b88cdb3e144dd890db20a72e3975a159f
bussiere <bussiere@gmail.com>
2025-11-27 11:58:28 +0100

    wip main Updated: 2025-11-27 11:58:28

--- run_ligne.sh
+++ run_ligne.sh
@@ -1,6 +1,6 @@
-rm pyproject.toml
-rm poetry.lock
-poetry init
-poetry add requests flask
-poetry update
+#rm pyproject.toml
+#rm poetry.lock
+#poetry init
+#poetry add requests flask
+#poetry update
 poetry run python ligne/ligne.py
