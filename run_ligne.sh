740f622844ed027ca9a29ffb0027c5502ee485d7
bussiere <bussiere@gmail.com>
2025-11-27 11:46:29 +0100

    wip main Updated: 2025-11-27 11:46:28

--- run_ligne.sh
+++ run_ligne.sh
@@ -1,2 +1,6 @@
+rm pyproject.toml
+rm poetry.lock
+poetry init
+poetry add requests flask
 poetry update
 poetry run python ligne/ligne.py
---
+++ run_survey.sh
@@ -0,0 +1,6 @@
+rm pyproject.toml
+rm poetry.lock
+poetry init
+poetry add requests flask
+poetry update
+poetry run python surveillance/survey.py
